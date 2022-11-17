c5761716d1a6d8c6c38cf5173228b638ca32ba00
nhat.bizz <nhattq.coding@gmail.com>
2022-11-17 10:20:56 +0700

    partytown astro base

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config'
 import svelte from '@astrojs/svelte'
 import node from '@astrojs/node'
 import AstroUnocss from '@unocss/astro';
-
+import AstroPartytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,5 +15,11 @@ export default defineConfig({
   adapter: node({
     mode: 'standalone',
   }),
-  integrations: [AstroUnocss({ configFile: './unocss.config.ts' }), svelte()],
+  integrations: [
+    AstroUnocss({ configFile: './unocss.config.ts' }), 
+    AstroPartytown({ 
+      config: { debug: true, forward: ['dataLayer.push'] }
+    }),
+    svelte()
+],
 })
